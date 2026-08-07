5ffa40e138a8d599d924f2e3d6310c4a9102210d
fsdavi13 <fsdavi13@gmail.com>
2026-08-06 23:20:08 -0300

    chore(frontend): configurar url da api por ambiente

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://192.168.0.101:8000",
+  baseURL:
+   import.meta.env.VITE_API_URL ??
+   "http://192.168.0.101:8000",
 });
 
 export default api;
